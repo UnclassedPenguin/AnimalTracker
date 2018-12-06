ba9ccdde2ac8db17333eed2ad831fe208da13b6c
UnclassedPenguin <tvanden@live.com>
2018-12-06 02:38:35 -0700

    added alternate ui

---
+++ UI-Alternate/tablewindow.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'tablewindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+'''
+Copyright © 2018 UnclassedPenguin
+App: Animal Tracker
+Author: UnclassedPenguin
+Description: An app to keep track of your Animals
+'''
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(600, 400)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.scroll = QtWidgets.QScrollArea(self.centralwidget)
+        self.scroll.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.scroll.setLineWidth(1)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setObjectName("scroll")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 582, 360))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.table = QtWidgets.QTableWidget(self.scrollAreaWidgetContents)
+        self.table.setObjectName("table")
+        self.table.setColumnCount(0)
+        self.table.setRowCount(0)
+        self.verticalLayout_2.addWidget(self.table)
+        self.scroll.setWidget(self.scrollAreaWidgetContents)
+        self.verticalLayout.addWidget(self.scroll)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 600, 22))
+        self.menuBar.setObjectName("menuBar")
+        self.menuFile = QtWidgets.QMenu(self.menuBar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menuBar)
+        self.actionClose = QtWidgets.QAction(MainWindow)
+        self.actionClose.setObjectName("actionClose")
+        self.actionClose_2 = QtWidgets.QAction(MainWindow)
+        self.actionClose_2.setObjectName("actionClose_2")
+        self.actionSave_As = QtWidgets.QAction(MainWindow)
+        self.actionSave_As.setObjectName("actionSave_As")
+        self.menuFile.addAction(self.actionSave_As)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionClose_2)
+        self.menuFile.addSeparator()
+        self.menuFile.addSeparator()
+        self.menuFile.addSeparator()
+        self.menuBar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Animal Tracker - Search"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.actionClose.setText(_translate("MainWindow", "Exit"))
+        self.actionClose_2.setText(_translate("MainWindow", "Close"))
+        self.actionSave_As.setText(_translate("MainWindow", "Save As..."))
+
